f7835c5570a68d6d2be62a03d4595bf01d2145b1
Larsluph <remarso59@sfr.fr>
2022-05-11 22:22:38 +0200

    First stable

--- app/src/main/java/com/larsluph/notifsender/MainActivity.kt
+++ app/src/main/java/com/larsluph/notifsender/MainActivity.kt
@@ -1,21 +1,62 @@
 package com.larsluph.notifsender
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+    private val channelId = "Channel ID"
+    private val channelName = "Channel Name"
+    private val channelDescription = "Channel Description"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.sendNotif).setOnClickListener {
+
+            sendNotif(
+                findViewById<TextView>(R.id.textTitle).text.toString(),
+                findViewById<TextView>(R.id.textContent).text.toString()
+            )
+        }
     }
 
-    fun sendNotif(title: String, content: String) {
-        val intent = Intent(this, SendNotifActivity::class.java).apply {
-            putExtra("title", title)
-            putExtra("content", content)
+    private fun createNotificationChannel(): String {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+            // Register the channel with the system
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
-        startActivity(intent)
+
+        return channelId
+    }
+
+    private fun sendNotif(title: String, content: String) {
+        val builder = NotificationCompat.Builder(this, createNotificationChannel())
+            .setContentTitle(title)
+            .setContentText(content)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content))
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(0, builder.build())
+        }
     }
 }
